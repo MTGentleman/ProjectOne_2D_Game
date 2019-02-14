6a762be021aa864d6c19a3f4261b720717bf9f6e
MTGentleman <43678373+MTGentleman@users.noreply.github.com>
2019-02-14 15:48:18 +0000

    Add files via upload

---
+++ EnemySpawnerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject EnemyDuck;
+    float randX;
+    Vector2 spawnLocation;
+    public float spawnRate = 4f;
+    float nextSpawn = 0.0f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(-7f, 6.5f);
+            spawnLocation = new Vector2(randX, transform.position.y);
+            Instantiate(EnemyDuck, spawnLocation, Quaternion.identity);
+        }
+
+	}
+}
